d85f2ec35532a9a682b7d75a0a1d6f20af451caa
Fepozopo <fepozopo.f39@gmail.com>
2024-09-28 19:28:41 -0700

    UPDATED src/main.py with better comments

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 from textnode import TextNode
 
 
-# Create a new TextNode object
+# Define the main function
 def main():
+    # Create a new TextNode object
     text = "This is a text node"
     text_type = "bold"
     url = "https://www.boot.dev"
     text_node = TextNode(text, text_type, url)
     print(text_node)
+
 
 
 # Call the main function
