d5e98abca53495a84268c80664a47fbc149ef4b7
Fepozopo <fepozopo.f39@gmail.com>
2024-10-03 18:18:54 -0700

    UPDATED src/main.py with the function copy_static_to_public(), CREATED the static directory, and UPDATED the .gitignore to include the public directory

--- src/main.py
+++ src/main.py
@@ -1,14 +1,32 @@
 from textnode import TextNode
+import os
 
 
 # Define the main function
 def main():
-    # Create a new TextNode object
-    text = "This is a text node"
-    text_type = "bold"
-    url = "https://www.boot.dev"
-    text_node = TextNode(text, text_type, url)
-    print(text_node)
+    
+    copy_static_to_public()
+
+
+def copy_static_to_public():
+    '''
+    This function copies all the contents of the static directory to the public directory recursively.
+    '''
+
+    # Create variables for the paths to the static and public directories
+    static_path = "static"
+    public_path = "public"
+
+    # First, delete all the contents of the public directory to ensure that the copy is clean
+    if os.path.exists(public_path):
+        os.system(f"rm -rf {public_path}/*")
+
+    # Create the public directory if it doesn't exist
+    if not os.path.exists(public_path):
+        os.mkdir(public_path)
+
+    # Then, copy the contents of the static directory to the public directory
+    os.system(f"cp -r {static_path}/* {public_path}")
 
 
 
